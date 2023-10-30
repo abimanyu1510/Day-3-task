510dfcb63b94624d7812ff02bfcfeb3f0fb9c0ce
abimanyu1510 <asalabimanyu15@gmail.com>
2023-10-30 14:38:34 +0530

    task updated

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let obj1={name:"persion 1",age:5};
 let obj2={age:5,name:"persion 1"};
 let json1=JSON.stringify(obj1);
 let json2=JSON.stringify(obj2);
-if(json1===json2);
+if(json1===json2)
 {
     console.log("both are equal");
 }
